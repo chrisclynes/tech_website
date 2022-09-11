78612a7d9bda0f637919acf69f88d8fba2661fb8
Chris Clynes <chrisclynes@live.com>
2022-09-11 19:03:09 -0400

    fixed jquery scrolling functions for window width account

--- js/index.js
+++ js/index.js
@@ -1,19 +1,24 @@
 $(document).ready(function(){
+    let windowWidth = $(window).width();
+    //set window width 
+    $(window).on('resize', function() {    
+        windowWidth = $(window).width();
+    })
     //nav menu btn
     $('#navbar-menu-btn').click(function(){
         $('.navbar-collapse').slideToggle(250);
     });
-
+    //window scrolling
     $(window).scroll(function(){
         let position = $(window).scrollTop();
         const trans = 200;
-        if(position > 100){
+        if(position > 100 && windowWidth >= 1024){
             $('.navbar-right-side-content, .navbar-left-side-content').hide(trans);
             $('.logo-link').hide(trans);
-        }else {
+        }else if (windowWidth >= 1024) {
             $('.navbar-right-side-content, .navbar-left-side-content').show(trans);
             $('.logo-link').show(trans);
-        }
+        }else return
     });
 });
 

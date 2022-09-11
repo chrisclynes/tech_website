cf5ebfab9b0104113b26739e4182849f78ea3270
Chris Clynes <chrisclynes@live.com>
2022-09-11 17:58:46 -0400

    navbar side content added, jquery scroll animation for navbar added.

--- js/index.js
+++ js/index.js
@@ -3,4 +3,17 @@ $(document).ready(function(){
     $('#navbar-menu-btn').click(function(){
         $('.navbar-collapse').slideToggle(250);
     });
+
+    $(window).scroll(function(){
+        let position = $(window).scrollTop();
+        const trans = 200;
+        if(position > 100){
+            $('.navbar-right-side-content, .navbar-left-side-content').hide(trans);
+            $('.logo-link').hide(trans);
+        }else {
+            $('.navbar-right-side-content, .navbar-left-side-content').show(trans);
+            $('.logo-link').show(trans);
+        }
+    });
 });
+
